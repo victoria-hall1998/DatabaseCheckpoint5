3328fc93ca846dd43b2077611f81a3f97ebe2433
Victoria Hall <victoria.hall20@yahoo.com>
2021-11-07 01:48:19 -0400

    Update CSE3241IOUtil.java

--- Checkpoint5/src/osu/cse3241/CSE3241IOUtil.java
+++ Checkpoint5/src/osu/cse3241/CSE3241IOUtil.java
@@ -1,12 +1,15 @@
 package osu.cse3241;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class CSE3241IOUtil {
 	/**
 	 * This class is used as a helper to check the input of the user before 
 	 */
 	
 	private CSE3241IOUtil() {
-    }
+    	}
 	
 	/**
 	 * Checks if the Work_ID input is 15 numbers
@@ -42,5 +45,39 @@ public class CSE3241IOUtil {
 		return value.length() > maxlength;
 	}
 	
+	/**
+	 * Checks the formatting of a double. Must be "0.0".
+	 */
+	public static boolean checkDoubleFormatting(String value) {
+		boolean makedoub = true;
+		boolean betweenvals = false;
+		Double doubval = null;
+		try {
+			doubval = Double.parseDouble(value);
+		}
+		catch (NumberFormatException e) {
+			makedoub = false;
+		}
+		
+		if (makedoub) {
+			betweenvals = 0.0 <= doubval && doubval <= 10.0;
+		}
+		
+		return betweenvals && makedoub;
+	}
 	
+	/**
+	 * Check if the date is formatted properly
+	 */
+	public static boolean checkDateFormat(String date) {
+		boolean formattedProperly = true;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        	dateFormat.setLenient(false);
+        	try {
+            	dateFormat.parse(date.trim());
+        	} catch (ParseException pe) {
+        	    formattedProperly = false;
+        	}
+        	return formattedProperly;
+	}
 }
